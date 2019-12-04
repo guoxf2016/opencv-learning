d1426f9064568681b0da48797e22d91bd9e98c8a
guoxf2016 <guoxiaofei2016@gmail.com>
2019-12-04 16:23:18 +0800

    example1, display a video

---
+++ example/example1/example1.cpp
@@ -0,0 +1,38 @@
+﻿// example1.cpp: 定义应用程序的入口点。
+//
+
+#include "example1.h"
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char ** argv)
+{
+	cv::namedWindow("Example1", cv::WINDOW_AUTOSIZE);
+	cv::VideoCapture cap;
+	if (argc == 1) {
+		cap.open(0);
+	}
+	else {
+		cap.open(argv[1]);
+	}
+
+	if (!cap.isOpened()) {
+		cerr << "Could not open capture!" << endl;
+		return -1;
+	}
+	cv::Mat frame;
+	for (;;) {
+		cap >> frame;
+		if (frame.empty()) {
+			break;
+		}
+		cv::imshow("Example1", frame);
+		if (cv::waitKey(33) >= 0) {
+			break;
+		}
+	}
+	//cout << "Hello CMake." << endl;
+	return 0;
+}
