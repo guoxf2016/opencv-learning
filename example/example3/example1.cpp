187a20b77eddf9b793e607f9f268219aa409ae9f
guoxf2016 <guoxiaofei2016@gmail.com>
2019-12-20 15:06:29 +0800

    example3 filter, example4 transform

---
+++ example/example3/example1.cpp
@@ -0,0 +1,95 @@
+﻿// example1.cpp: 定义应用程序的入口点。
+//
+
+#include "example1.h"
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace std;
+
+bool matIsEqual(const cv::Mat mat1, const cv::Mat mat2);
+
+int main(int argc, char** argv)
+{
+
+	/*
+	1. Load an image with interesting textures. Smooth the image in several ways using
+cv::smooth() with smoothtype=cv::GAUSSIAN.
+a. Use a symmetric 3 × 3, 5 × 5, 9 × 9, and 11 × 11 smoothing window size and
+display the results.
+b. Are the output results nearly the same by smoothing the image twice with a
+5 × 5 Gaussian filter as when you smooth once with two 11 × 11 filters? Why
+or why not?
+	*/
+
+	cv::namedWindow("Example3", cv::WINDOW_AUTOSIZE);
+
+	cv::Mat oriImage = cv::imread(argv[1]);
+
+	cv::imshow("Example3", oriImage);
+
+	cv::namedWindow("smooth 3x3", cv::WINDOW_AUTOSIZE);
+
+	cv::Mat blur33(oriImage.size(), CV_16S, cv::Scalar(0, 0, 0));
+
+	cv::blur(oriImage, blur33, cv::Size2d(3, 3));
+
+	cv::imshow("smooth 3x3", blur33);
+
+	cv::namedWindow("smooth 11x11", cv::WINDOW_AUTOSIZE);
+
+	cv::Mat blur11x11(oriImage.size(), CV_16S, cv::Scalar(0, 0, 0));
+
+	cv::blur(oriImage, blur11x11, cv::Size2d(11, 11));
+
+	cv::imshow("smooth 11x11", blur11x11);
+
+	cv::Mat blur55(oriImage.size(), CV_16S, cv::Scalar(0, 0, 0));
+	cv::Mat blur55_55(oriImage.size(), CV_16S, cv::Scalar(0, 0, 0));
+	cv::blur(oriImage, blur55, cv::Size2d(5, 5));
+	cv::blur(blur55, blur55_55, cv::Size2d(5, 5));
+
+	cout << "twice 5x5 and once 11x11 is same? " << matIsEqual(blur55_55, blur11x11) << endl;
+
+
+	/*
+	2. Create a 100 × 100 single-channel image. Set all pixels to 0. Finally, set the center
+pixel equal to 255.
+a. Smooth this image with a 5 × 5 Gaussian filter and display the results. What
+did you find?
+b. Do this again but with a 9 × 9 Gaussian filter.
+c. What does it look like if you start over and smooth the image twice with the 5
+× 5 filter? Compare this with the 9 × 9 results. Are they nearly the same? Why
+or why not?
+
+	*/
+
+	cv::Mat mat100x100(100, 100, CV_8UC1, cv::Scalar(0));
+	mat100x100.at<uchar>(50, 50) = 255;
+	cv::Mat blur100x100(mat100x100.size(), CV_16SC1, cv::Scalar(0));
+	cv::GaussianBlur(mat100x100, blur100x100, cv::Size(5, 5), 0, 0);
+
+	cv::namedWindow("blur100x100", cv::WINDOW_AUTOSIZE);
+	cv::imshow("blur100x100", blur100x100);
+
+	cv::waitKey(0);
+	//cout << "Hello CMake." << endl;
+	return 0;
+}
+
+bool matIsEqual(const cv::Mat mat1, const cv::Mat mat2) {
+	if (mat1.empty() && mat2.empty()) {
+		return true;
+	}
+	if (mat1.cols != mat2.cols || mat1.rows != mat2.rows || mat1.dims != mat2.dims ||
+		mat1.channels() != mat2.channels()) {
+		return false;
+	}
+	if (mat1.size() != mat2.size() || mat1.channels() != mat2.channels() || mat1.type() != mat2.type()) {
+		return false;
+	}
+	int nrOfElements1 = mat1.total() * mat1.elemSize();
+	if (nrOfElements1 != mat2.total() * mat2.elemSize()) return false;
+	bool lvRet = memcmp(mat1.data, mat2.data, nrOfElements1) == 0;
+	return lvRet;
+}
---
+++ example/example4/example1.cpp
@@ -0,0 +1,112 @@
+﻿// example1.cpp: 定义应用程序的入口点。
+//
+
+#include "example1.h"
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace std;
+
+bool matIsEqual(const cv::Mat mat1, const cv::Mat mat2);
+
+int main(int argc, char** argv)
+{
+
+	if (argc != 2) {
+		cout << "Usage: " << argv[0] << " <iamgename>\n" << endl;
+		return -1;
+	}
+
+	cv::Mat src = cv::imread(argv[1]);
+	if (src.empty()) {
+		cout << "can not load " << argv[1] <<endl;
+		return -1;
+	}
+
+	cv::Point2f srcTri[] = {
+		cv::Point2f(0, 0 ),
+		cv::Point2f(src.cols - 1, 0),
+		cv::Point2f(0, src.rows - 1)
+	};
+
+	cv::Point2f dstTri[] = {
+		cv::Point2f(src.cols * 0.f, src.rows * 0.33f), // dst Top left
+		cv::Point2f(src.cols * 0.85f, src.rows * 0.25f), // dst Top right
+		cv::Point2f(src.cols * 0.15f, src.rows * 0.7f) // dst Bottom left
+	};
+
+	cv::Mat warp_mat = cv::getAffineTransform(srcTri, dstTri);
+
+	cv::Mat dst, dst2;
+
+	cv::warpAffine(src, dst, warp_mat, src.size(), cv::INTER_LINEAR, cv::BORDER_CONSTANT);
+
+	for (int i = 0; i < 3; ++i)
+		cv::circle(dst, dstTri[i], 5, cv::Scalar(255, 0, 255), -1);
+	cv::imshow("Affine Transform Test", dst);
+
+	cv::waitKey(0);
+
+	for (int frame = 0; ; ++frame) {
+		cv::Point2f center(src.cols * 0.5f, src.rows * 0.5f);
+		double angle = frame * 3 % 360, scale = (cos((angle - 60) * CV_PI / 180) + 1.05) * 0.8;
+		cv::Mat rot_mat = cv::getRotationMatrix2D(center, angle, scale);
+		cv::warpAffine(
+			src,
+			dst,
+			rot_mat,
+			src.size(),
+			cv::INTER_LINEAR,
+			cv::BORDER_CONSTANT,
+			cv::Scalar()
+		);
+		cv::imshow("Rotated Image", dst);
+		if (cv::waitKey(30) >= 0)
+			break;
+
+	}
+
+	cv::Point2f srcQuad[] = {
+		cv::Point2f(0, 0), // src Top left
+		cv::Point2f(src.cols - 1, 0), // src Top right
+		cv::Point2f(src.cols - 1, src.rows - 1), // src Bottom right
+		cv::Point2f(0, src.rows - 1) // src Bottom left
+	};
+
+	cv::Point2f dstQuad[] = {
+		cv::Point2f(src.cols * 0.05f, src.rows * 0.33f),
+		cv::Point2f(src.cols * 0.9f, src.rows * 0.25f),
+		cv::Point2f(src.cols * 0.8f, src.rows * 0.9f),
+		cv::Point2f(src.cols * 0.2f, src.rows * 0.7f)
+	};
+
+	cv::Mat warp_mat2 = cv::getPerspectiveTransform(srcQuad, dstQuad);
+
+	cv::warpPerspective(src, dst2, warp_mat2, src.size(), cv::INTER_LINEAR,
+		cv::BORDER_CONSTANT, cv::Scalar());
+
+	for (int i = 0; i < 4; i++)
+		cv::circle(dst2, dstQuad[i], 5, cv::Scalar(255, 0, 255), -1);
+	cv::imshow("Perspective Transform Test", dst2);
+
+	cv::waitKey(0);
+	//cout << "Hello CMake." << endl;
+	return 0;
+}
+
+bool matIsEqual(const cv::Mat mat1, const cv::Mat mat2) {
+	if (mat1.empty() && mat2.empty()) {
+		return true;
+	}
+	if (mat1.cols != mat2.cols || mat1.rows != mat2.rows || mat1.dims != mat2.dims ||
+		mat1.channels() != mat2.channels()) {
+		return false;
+	}
+	if (mat1.size() != mat2.size() || mat1.channels() != mat2.channels() || mat1.type() != mat2.type()) {
+		return false;
+	}
+	int nrOfElements1 = mat1.total() * mat1.elemSize();
+	if (nrOfElements1 != mat2.total() * mat2.elemSize()) return false;
+	bool lvRet = memcmp(mat1.data, mat2.data, nrOfElements1) == 0;
+	return lvRet;
+}
